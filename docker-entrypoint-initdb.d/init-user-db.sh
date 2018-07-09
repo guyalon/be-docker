7abb69e0a468f640e2328d9ac5cc5936daa4d39e
Guy Alon <guy_alon@yahoo.com>
2018-07-09 16:44:43 +0300

    shell sql check

--- docker-entrypoint-initdb.d/init-user-db.sh
+++ docker-entrypoint-initdb.d/init-user-db.sh
@@ -2,7 +2,47 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER docker;
-    CREATE DATABASE docker;
-    GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+    CREATE USER pg_user;
+    DROP DATABASE IF EXISTS twitter;
+    CREATE DATABASE twitter;
+    GRANT ALL PRIVILEGES ON DATABASE twitter TO pg_user;
+
+    \c twitter;
+
+    CREATE TABLE tweets (
+      ID SERIAL PRIMARY KEY,
+      content VARCHAR(140),
+      username VARCHAR(20),
+      likes_count INTEGER,
+      retweet_count INTEGER,
+      created_at DATE,
+      updated_at DATE
+    );
+
+    CREATE TABLE retweets (
+      retweet_id SERIAL PRIMARY KEY,
+      content VARCHAR(140),
+      tweet_user VARCHAR(20),
+      tweet_id INTEGER references tweets(ID),
+      created_at DATE,
+      updated_at DATE
+    );
+
+    CREATE TABLE likes (
+      like_user VARCHAR(20),
+      tweet_id INTEGER references tweets(ID),
+      created_at DATE,
+      updated_at DATE
+    );
+
+
+    INSERT INTO tweets (content, username, likes_count, retweet_count, created_at, updated_at)
+      VALUES ('This is all i have to say', 'TweeterGuy', 1, 1,now(),now());
+
+    INSERT INTO retweets (content, tweet_user, tweet_id, created_at, updated_at)
+      VALUES ('No way you cannot say anything else', 'TweeterDude', 1, now(),now());
+
+    INSERT INTO likes (like_user, tweet_id, created_at, updated_at)
+      VALUES ('TweeterBro', 1, now(),now());
+
 EOSQL
