f1b4f39eb93e1f92703f2b278eca9a1d73a08eca
Guy Alon <guy_alon@yahoo.com>
2018-07-09 16:12:14 +0300

    bla

--- app.js
+++ app.js
@@ -27,26 +27,17 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status( err.code || 500 )
-            .json({
-                status: 'error',
-                message: err
-            });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// error handler
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500)
-        .json({
-            status: 'error',
-            message: err.message
-        });
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+
 
 module.exports = app;
